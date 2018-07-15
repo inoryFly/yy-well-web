1141597c44243e97a98be384428eaf153a4aef8e
inoryFly <17611233352@163.com>
2018-07-16 01:28:29 +0800

    ok

---
+++ src/pages/mobile/record/record.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="nav">
+            <span class="active">全部</span>
+            <span class="active">待发币</span>
+            <span class="active">已完成</span>
+        </div>
+
+        <div class="page-tab-container">
+            
+        </div>
+    </div>
+</template>
