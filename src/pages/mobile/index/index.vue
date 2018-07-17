869abb99e765a49b4b30534122e37bb3a30220c4
inory <17611233352@163.com>
2018-07-17 19:00:32 +0800

    d

--- src/pages/mobile/index/index.vue
+++ src/pages/mobile/index/index.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        
+   <div class="page-tabbar">
+    <div class="page-wrap">
+      <div class="page-title">Tabbar</div>
+      <div>
+        <mt-cell class="page-part" title="当前选中" :value="selected" />
+      </div>
+
+      
     </div>
+
+    <mt-tabbar v-model="selected" fixed>
+      <mt-tab-item id="项目列表">
+        <img slot="icon" src="../../../images/project.png">
+        项目列表
+      </mt-tab-item>
+      <mt-tab-item id="众筹记录">
+        <img slot="icon" src="../../../images/record.png">
+        众筹记录
+      </mt-tab-item>
+      <mt-tab-item id="个人中心">
+        <img slot="icon" src="../../../images/person.png">
+        个人中心
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'page-tabbar',
+  data() {
+    return {
+      selected: '项目列表'
+    };
+  }
+};
+</script>
+
+<style>
+  .page-tabbar {
+    overflow: hidden;
+    height: 100vh;
+  }
+  .page-wrap {
+    overflow: auto;
+    height: 100%;
+    padding-bottom: 100px;
+  }
+</style>
